f9d8edb410ba787298a8357e2ba481b6b295ef2f
Valerie Hammond <luckbit4755+git-em@gmail.com>
2020-12-14 16:00:03 -0500

    whitespace

--- docker/splash.sh
+++ docker/splash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash	
 
 # from splash-show.sh ....
-
 export SPLASH_IMAGE="/usr/local/bin/splash.png"
+
 _splash_main() {
 	echo reading geometry
 	read screen_width screen_height < <(xdotool getdisplaygeometry)
@@ -13,15 +13,15 @@ _splash_main() {
 		sed 's/.* \([0-9]*\)x\([0-9]*\) .*/\1 \2/g')
 	echo read image size as ${image_width} ${image_height}
 
-    splash_width=$image_width
-    splash_width=${image_height}
-    image_clip_x=$((screen_width/2-image_width/2))
-    image_clip_y=$((screen_height/2-image_height/2))
-    image_clip=${image_width}x${image_height}+${image_clip_x}+${image_clip_y}
+	splash_width=$image_width
+	splash_width=${image_height}
+	image_clip_x=$((screen_width/2-image_width/2))
+	image_clip_y=$((screen_height/2-image_height/2))
+	image_clip=${image_width}x${image_height}+${image_clip_x}+${image_clip_y}
 
 	# on the background 
 	echo xview -center -onroot $SPLASH_IMAGE
-    xview -center -onroot $SPLASH_IMAGE
+	xview -center -onroot $SPLASH_IMAGE
 
 	# in the foreground
 	echo xview $SPLASH_IMAGE -geometry $image_clip 
