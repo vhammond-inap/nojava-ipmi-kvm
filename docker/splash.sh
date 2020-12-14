29cec9f49c70e5489daa11e6067a51c7b23cd754
Valerie Hammond <luckbit4755+git-em@gmail.com>
2020-12-14 15:58:50 -0500

    remove debug logs

--- docker/splash.sh
+++ docker/splash.sh
@@ -13,40 +13,27 @@ _splash_main() {
 		sed 's/.* \([0-9]*\)x\([0-9]*\) .*/\1 \2/g')
 	echo read image size as ${image_width} ${image_height}
 
-echo a1
     splash_width=$image_width
-echo a2 ${splash_width}
     splash_width=${image_height}
-echo a3 ${image_height}
     image_clip_x=$((screen_width/2-image_width/2))
-echo a4 ${image_clip_x}
     image_clip_y=$((screen_height/2-image_height/2))
-echo a5 ${image_clip_y}
     image_clip=${image_width}x${image_height}+${image_clip_x}+${image_clip_y}
-echo a6 ${image_clip}
 
 	# on the background 
 	echo xview -center -onroot $SPLASH_IMAGE
     xview -center -onroot $SPLASH_IMAGE
 
-	if [ 0 = 1 ] ; then 
-		while true ; do
-			sleep 13
-		done
-	else
-		# in the foreground
-		echo xview $SPLASH_IMAGE -geometry $image_clip 
-		xview $SPLASH_IMAGE -geometry $image_clip &
-		echo xview is running as ${xview_pid}
+	# in the foreground
+	echo xview $SPLASH_IMAGE -geometry $image_clip 
+	xview $SPLASH_IMAGE -geometry $image_clip &
+	echo xview is running as ${xview_pid}
 
-		xview_winid=$(xdotool search --sync --name $SPLASH_IMAGE)
-		echo xview_winid is ${xview_winid}
-		while xdotool windowraise $xview_winid ; do 
-			true
-			sleep 1
-		done
-		echo oof...
-	fi
+	xview_winid=$(xdotool search --sync --name $SPLASH_IMAGE)
+	echo xview_winid is ${xview_winid}
+	while xdotool windowraise $xview_winid ; do 
+		true
+		sleep 1
+	done
 }
 
 _splash_main ${*}
